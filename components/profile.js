19b032a2bc0535e729f80f70a4b41fdc8499212d
Inanc <inancakduvann@gmail.com>
2020-05-03 16:28:47 +0300

    map added to profile page

--- components/profile.js
+++ components/profile.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons";
+import { WebView } from 'react-native-webview';
+
 
 export default function Home() {
     return (
@@ -18,6 +20,25 @@ export default function Home() {
                     <View style={styles.profileInfo}>
                         <Text style={styles.profileInfoHeading}>Name</Text>
                         <Text style={styles.profileInfoName}>Ahmet Mümtaz</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <WebView
+                        source={{ html: '<iFrame src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.019327543697!2d28.99383995145631!3d41.04670642490543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab70a6056ba9f%3A0xd9d0ab19bd17d964!2zxZ5pxZ9saSDDlsSfcmV0bWVuZXZp!5e0!3m2!1str!2str!4v1588511742606!5m2!1str!2str" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iFrame>' }}
+                        style={{ marginTop: 20, width: "100%", height: 300 }}
+                    />
+                </View>
+
+                <View style={styles.profileInfoDetail}>
+                    <View style={styles.profileInfoDetailSection}>
+                        <Text style={styles.profileInfoDetailHeading}>Gender</Text>
+                        <Text style={styles.profileInfoDetailDesc}>Male</Text>
+                    </View>
+
+                    <View style={styles.profileInfoDetailSection}>
+                        <Text style={styles.profileInfoDetailHeading}>Gender</Text>
+                        <Text style={styles.profileInfoDetailDesc}>Male</Text>
                     </View>
                 </View>
             </ScrollView>
@@ -59,5 +80,15 @@ const styles = StyleSheet.create({
         fontSize: 24,
         color: "#8895f5",
         fontWeight: "bold"
+    },
+    profileInfoDetail: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    profileInfoDetailSection: {
+        width: "50%",
+        padding: 10
     }
+
 });
