31768b876bb2b6ecb9578956a5b59b77e1e86a4b
Inanc <inancakduvann@gmail.com>
2020-05-05 21:00:00 +0300

    unused imports removed

--- App.js
+++ App.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import Navigator from "./routers/routerStack";
 
-// Components
-import Home from "./components/home";
-import Profile from "./components/profile";
-
 export default function App() {
-
-  // const [users, setUsers] = useState([]);
-
-  // const ws = new WebSocket(
-  //   "https://wunder-provider.herokuapp.com/"
-  // );
-
-  // ws.onopen = () => {
-  //   console.log("Opened.");
-  // };
-
-  // ws.onmessage = msg => {
-  //   console.log(msg, "saa")
-  // };
 
   return (
     <Navigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/profile.js
+++ components/profile.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons";
 import { WebView } from 'react-native-webview';
 
